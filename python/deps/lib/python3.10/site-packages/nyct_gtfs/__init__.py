566f055ffbc75f95bfe3549dd1336b5d0035b9c6
Mihir Shetty <ms6556@columbia.edu>
2023-12-30 06:25:51 -0500

    Added. gitignore to not include venv stuff

--- python/deps/lib/python3.10/site-packages/nyct_gtfs/__init__.py
+++
@@ -1,4 +0,0 @@
-from nyct_gtfs.feed import NYCTFeed
-from nyct_gtfs.trip import Trip
-from nyct_gtfs.stop_time_update import StopTimeUpdate
-from nyct_gtfs.gtfs_static_types import Stations, TripShapes
